2ed05ea3be89b8cd193a034c3f063397d3f175f0
Gerardo Caceres <gpcaceres@teco.com.ar>
2019-09-08 06:47:48 -0300

    add unique url link to mail

--- src/main/kotlin/flow/com/ar/recruiter/service/MailContentBuilder.kt
+++ src/main/kotlin/flow/com/ar/recruiter/service/MailContentBuilder.kt
@@ -1,6 +1,8 @@
 package flow.com.ar.recruiter.service
 
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context;
@@ -11,9 +13,14 @@ class MailContentBuilder {
     @Autowired
     val templateEngine: TemplateEngine? = null
 
+    @Value("\${mail.baseurl}")
+    val baseurl: String? = null
+
     fun build(message: String): String {
         val context = Context()
+        val urlquery = NanoIdUtils.randomNanoId();
         context.setVariable("message", message)
+        context.setVariable("link", "$baseurl$urlquery")
         return templateEngine!!.process("recruitment", context)
     }
 
